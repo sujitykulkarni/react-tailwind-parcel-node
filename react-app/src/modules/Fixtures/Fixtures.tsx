318e74c6a1cc38fe096c95de7357374f1efc5ed1
Sujit <sujit.kulkarni@rapidcircle.com>
2022-11-28 17:24:45 +0530

    fixtures visualizations

--- react-app/src/modules/Fixtures/Fixtures.tsx
+++ react-app/src/modules/Fixtures/Fixtures.tsx
@@ -1,13 +1,18 @@
-import React, { useCallback, useEffect, useMemo, useState } from "react";
+import React, { useCallback, useEffect, useMemo } from "react";
 import { useFixtureStore } from "../../store/fixture.store";
-import FixtureEventFlow from "../../components/EventFlowChart/FixtureEventFlow";
-import { Event } from "../../interfaces/events.interface";
+import EventFlowChart from "../../components/EventFlowChart/EventFlowChart";
+import ScoreGroupPie from "./Charts/ScoreGroupPie";
+import ScoreGroupBarChart from "./Charts/ScoreGroupBarChart";
 
 const Fixtures = () => {
-  const [gameWeeks, fetchGameWeeks] = useFixtureStore((state) => [
-    state.gameWeeks,
-    state.fetchGameWeeks,
-  ]);
+  const [gameWeeks, fixtures, fetchGameWeeks, fetchFixtures] = useFixtureStore(
+    (state) => [
+      state.gameWeeks,
+      state.fixtures,
+      state.fetchGameWeeks,
+      state.fetchFixtures,
+    ]
+  );
 
   const gameWeekEventFlowData = useMemo(
     () =>
@@ -19,9 +24,42 @@ const Fixtures = () => {
       })),
     [gameWeeks]
   );
+
+  const scoreHistogramData = useMemo(() => {
+    const scores = fixtures
+      .sort(
+        (a, b) =>
+          a.team_h_score - b.team_h_score || a.team_a_score - b.team_a_score
+      )
+      .map((fixture) =>
+        fixture.finished
+          ? `${fixture.team_h_score}-${fixture.team_a_score}`
+          : null
+      );
+    const scoreGroups = scores.reduce(
+      (acc: { [key: string]: number }, cur: string | null) => {
+        if (cur) {
+          if (!acc[cur]) {
+            acc[cur] = 1;
+            return acc;
+          }
+          acc[cur] += 1;
+          return acc;
+        }
+        return acc;
+      },
+      {}
+    );
+    return Object.keys(scoreGroups).map((key) => ({
+      name: key,
+      value: scoreGroups[key],
+    }));
+  }, [fixtures]);
+
   const loadData = useCallback(async () => {
     await fetchGameWeeks();
-  }, [fetchGameWeeks]);
+    await fetchFixtures();
+  }, [fetchGameWeeks, fetchFixtures]);
 
   const init = useCallback(() => {
     if (!gameWeeks.length) {
@@ -32,11 +70,24 @@ const Fixtures = () => {
   useEffect(init, []);
 
   return gameWeekEventFlowData.length ? (
-    <div className="min-h-screen p-2">
-      <h1 className="text-2xl">
-        Fixtures sized by transfers made per game week.
-      </h1>
-      <FixtureEventFlow data={gameWeekEventFlowData} />
+    <div className="min-h-screen p-2 flex flex-col justify-start gap-4">
+      <div>
+        <h1 className="text-2xl">
+          Fixtures sized by transfers made per game week.
+        </h1>
+        <EventFlowChart data={gameWeekEventFlowData} />
+      </div>
+      <div>
+        <div className="flex justify-between">
+          <h1 className="text-2xl">Scores grouped by their count</h1>
+          <div>
+            <button>Pie</button>
+            <button>Bar</button>
+          </div>
+        </div>
+        {/* <ScoreGroupPie data={scoreHistogramData} /> */}
+        <ScoreGroupBarChart data={scoreHistogramData} />
+      </div>
     </div>
   ) : null;
 };
--- react-app/src/store/fixture.store.ts
+++ react-app/src/store/fixture.store.ts
@@ -1,19 +1,27 @@
 import create from "zustand";
-import { fetchGameWeeks as fetchGameWeeksApi } from "../api/api";
+import { Fixture } from "~/interfaces/fixtures.interface";
+import { fetchFixtures, fetchGameWeeks as fetchGameWeeksApi } from "../api/api";
 import { Event } from "../interfaces/events.interface";
 
 type State = {
   gameWeeks: Event[];
+  fixtures: Fixture[];
 };
 
 type Action = {
   fetchGameWeeks: () => Promise<void>;
+  fetchFixtures: () => Promise<void>;
 };
 
 export const useFixtureStore = create<State & Action>((set) => ({
   gameWeeks: [],
+  fixtures: [],
   fetchGameWeeks: async () => {
     const gameweeks = await fetchGameWeeksApi();
     set({ gameWeeks: gameweeks });
+  },
+  fetchFixtures: async () => {
+    const fixtures = await fetchFixtures();
+    set({ fixtures });
   },
 }));
