605bfdc18296baf929e546190bb0823cfe1793f1
Sujit <sujit.kulkarni@rapidcircle.com>
2022-12-21 22:13:27 +0530

    Move backend (api) to new repo

--- api/src/cache/cache.js
+++
@@ -1,8 +0,0 @@
-const NodeCache = require("node-cache");
-module.exports = new NodeCache();
-
-const CACHE_KEYS = {
-  TEAMS: "teams",
-  EVENTS: "events",
-};
-module.exports.CACHE_KEYS = CACHE_KEYS;
