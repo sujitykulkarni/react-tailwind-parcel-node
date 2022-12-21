93460442b8c0662e9f8df5122225ba1a1fbb591f
Sujit <sujit.kulkarni@rapidcircle.com>
2022-12-21 19:14:50 +0530

    Support theme colors on button through intents

--- react-app/src/components/Button/Button.tsx
+++ react-app/src/components/Button/Button.tsx
@@ -1,12 +1,26 @@
 import classNames from "classnames";
 import React from "react";
 
-type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  intent?: "primary" | "secondary" | "success" | "error" | "warning";
+};
 
-const Button: React.FC<ButtonProps> = ({ className, ...props }) => {
+const Button: React.FC<ButtonProps> = ({
+  className,
+  intent = "secondary",
+  ...props
+}) => {
   const classes = classNames(
-    "p-2 rounded text:white hover:text-blue-500 bg-stone-100 border-b-2 border-r-2 border-stone-200 active:bg-stone-300",
-    className
+    "p-3 rounded-md text-base text-white active:bg-stone-300 hover:shadow-lg hover:shadow-indigo-500/40 ",
+    className,
+    {
+      "bg-primary": intent === "primary",
+      "bg-secondary": intent === "secondary",
+      "bg-success": intent === "success",
+      "bg-warning": intent === "warning",
+      "bg-error": intent === "error",
+      "bg-stone-100": !intent,
+    }
   );
   return <button {...props} className={classes} />;
 };
