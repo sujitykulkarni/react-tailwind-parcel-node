5dc628a69b41e3c020e4813d4b3feca7e179a98e
Sujit <sujit.kulkarni@rapidcircle.com>
2022-12-29 22:39:17 +0530

    body classname prop for Card

--- react-app/src/components/Card/Card.tsx
+++ react-app/src/components/Card/Card.tsx
@@ -1,16 +1,25 @@
+import classNames from "classnames";
 import React from "react";
 
 const Card: React.FunctionComponent<
-  React.PropsWithChildren & { title?: React.ReactNode }
-> = ({ children, title }) => {
+  React.PropsWithChildren &
+    Omit<React.HTMLProps<HTMLDivElement>, "title"> & {
+      title?: string | React.ReactElement;
+      bodyClasses?: string;
+    }
+> = ({ children, title, className, bodyClasses, ...restDivProps }) => {
+  const classes = classNames(
+    className,
+    "rounded-md border border-stone-200 shadow-md min-w-content"
+  );
   return (
-    <div className="rounded-md border border-stone-200 shadow-md">
+    <div {...restDivProps} className={classes}>
       {title && (
         <div className="bg-stone-50 border-stone-200 p-4 rounded-t-md border-b">
           <h3 className="text-lg font-medium">{title}</h3>
         </div>
       )}
-      <div className="p-4">{children}</div>
+      <div className={`p-4 ${bodyClasses}`}>{children}</div>
     </div>
   );
 };
